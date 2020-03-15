f617089eecda910fcc8f164ba36779f310653e8d
Alexei Ivkin <ivkin.alexey@yandex.ru>
2020-03-15 17:24:44 +0300

    feat: add slider animation

--- script.js
+++ script.js
@@ -22,3 +22,35 @@ function highlight(elem) {
     activeLink = elem;
     activeLink.classList.add('active');
 }
+
+/*---------------------------SLIDER--------------------------------*/
+let slideIndex = 1;
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName('slider_slide');
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+function setEventListeners() {
+    const next = document.getElementsByClassName('slider_link-next')[0],
+        prev = document.getElementsByClassName('slider_link-prev')[0];
+    next.addEventListener('click', () => plusSlides(1));
+    prev.addEventListener('click', () => plusSlides(-1));
+}
+
+setEventListeners();
+showSlides(slideIndex);
